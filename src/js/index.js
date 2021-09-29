aaef4b990898ed0de076f386df33364d35f68c6e
Pawel Switalski <pawelswitalski@gmail.com>
2021-09-29 14:54:05 +0200

    create scroll trigerred animations in gsap

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,84 @@
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+//  dropDown
+
 const dropDown = document.querySelector('.home__form');
 const signInButton = document.querySelector('.home__signin');
 
 signInButton.addEventListener('click', () => {
     dropDown.classList.toggle('home__form--active');
+})
+
+// animations
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to("#meditation", {
+    scrollTrigger: {
+        trigger: '#meditation',
+        start: "top center"
+    },
+    opacity: '1',
+    scaleX: '1',
+    delay: 0.2,
+    snap: {
+        duration: {
+            min: 0.5,
+            max: 1
+        }
+    }
+})
+
+gsap.to("#pricing", {
+    scrollTrigger: {
+        trigger: "#pricing",
+        start: "top center"
+    },
+    opacity: '1',
+    scaleX: '1',
+    delay: 0.2,
+    snap: {
+        duration: {
+            min: 0.5,
+            max: 1
+        }
+    }
+})
+
+gsap.to('.lake', {
+    scrollTrigger: {
+        trigger: ".lake",
+        start: "top bottom-=300px",
+    },
+    opacity: '1'
+})
+
+gsap.to('.links__container', {
+    scrollTrigger: {
+        trigger: ".links",
+        start: "top bottom-=200px",
+    },
+    opacity: '1',
+    scaleY: '1'
+})
+
+gsap.to('.footer__links', {
+    scrollTrigger: {
+        trigger: ".footer",
+        start: "top bottom",
+    },
+    opacity: '1',
+    scaleY: '1',
+    delay: 0.4,
+})
+
+gsap.to('.footer__copy', {
+    scrollTrigger: {
+        trigger: ".footer",
+        start: "top bottom",
+    },
+    opacity: '1',
+    scaleY: '1',
+    delay: 0.6,
 })
